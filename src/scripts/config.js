38963a0510e51466f7f02d540f98c42071487df9
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-06-01 10:54:37 +0200

    API_PATH correction - added slash at the end

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,4 +1,4 @@
-export const API_PATH = 'https://pixabay.com/api';
+export const API_PATH = 'https://pixabay.com/api/';
 export const API_KEY = '31641463-8cc19d34af378b8aeb6cde8f1';
 
 export const DEFAULT_PIXABAY_PARAMS = {
